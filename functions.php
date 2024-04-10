c97285b5804c8dc6c8bcae22639ca63c742e4bb7
Richie Arnold <richard@ambercouch.co.uk>
2024-04-10 13:20:26 +0100

    TIB - Added back button to blog and podcasts

--- functions.php
+++ functions.php
@@ -13,3 +13,22 @@ function avia_modify_post_grid_query_asc( $query ) {
     $query['order'] = 'ASC';
     return $query;
 }
+
+
+ add_filter('the_content', 'ac_blog_back');
+function ac_blog_back($content){
+    global $post;
+    $is_enfold_builder_active = get_post_meta($post->ID, '_aviaLayoutBuilder_active', true);
+
+    $output = "";
+    if (is_single() && 'active' === $is_enfold_builder_active){
+        $output .= "<div class='container'>";
+        $output .= "<div class='content u-mb-0 u-pb-0'>";
+        $output .= "<a class='c-btn--back' data-av_iconfont='entypo-fontello' data-av_icon='' href='/blog-page'> All Blogs</a>";
+        $output .= "</div>";
+        $output .= "</div>";
+    }
+    $output .= $content;
+
+    return $output;
+}
